a7beb4395ccaa5207485f9bcfb7939d4a3f5a40c
kitian616 <kitian616@outlook.com>
2018-07-29 21:39:40 +0800

    chore: merge to Text Theme

--- bin/merge.sh
+++ bin/merge.sh
@@ -1,3 +1,3 @@
-for f in _data _includes _layouts _sass assets 404.html favicon.ico; do
-    cp -Rfv ../jekyll-TeXt-theme/$f .;
+for f in _data/licenses.yml _data/locale.yml _data/variables.yml _includes _layouts _sass assets 404.html favicon.ico; do
+    cp -Rfv ../jekyll-TeXt-theme/$f $(dirname $f);
 done
