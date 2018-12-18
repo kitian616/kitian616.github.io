5cbd67f5a8b96efb5ca38ae30d22d2561fa5eb5e
kitian616 <kitian616@outlook.com>
2018-12-18 23:38:37 +0800

    feat: update index.html, add googlead

--- bin/merge.sh
+++ bin/merge.sh
@@ -1,3 +1,3 @@
 for f in _data/licenses.yml _data/locale.yml _data/variables.yml _includes _layouts _sass assets 404.html favicon.ico; do
-    cp -Rfv ../jekyll-TeXt-theme/$f $(dirname $f);
+  rsync -rv --exclude=custom* ../jekyll-TeXt-theme/$f $(dirname $f);
 done
